75445e43a9c780b17cf40d956d27a173567293f6
Kushagra Gupta <kushagra.kingg@live.com>
2024-04-18 18:08:24 +0530

    Update Weather.svelte

--- frontend/src/pages/Weather.svelte
+++ frontend/src/pages/Weather.svelte
@@ -2,7 +2,8 @@
   import { GetWeather } from "../../wailsjs/go/main/WeatherData.js";
   import { onMount } from "svelte";
 
-  let weatherjson = "";
+  let weatherjson;
+  let city = "";
 
   onMount(() => {
     getweatherdata();
@@ -20,6 +21,12 @@
 </script>
 
 <main>
-  Weather
-  <button on:click={printw}>print weather</button>
+  <div class="pt-24 flex flex-col items-center">
+    <input bind:value={city} type="text" />
+    <button on:click={printw}>print weather</button>
+    <button on:click={getweatherdata}>get weather</button>
+  </div>
+  <div>
+    <p bind:value={weatherjson.name}></p>
+  </div>
 </main>
